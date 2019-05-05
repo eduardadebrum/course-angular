d80673647dfee30eed32a1f82c53e4363d804724
dudabrum <eduardadebrum94@gmail.com>
2019-05-05 10:44:24 -0300

    Adicionando tela de detalhes.

--- src/app/restaurants/restauran-detail/restauran-detail.component.ts
+++ src/app/restaurants/restauran-detail/restauran-detail.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RestaurantService } from '../restaurant/restaurant.service';
+import { Restaurant } from '../restaurant/restaurant.model'
+import { ActivatedRoute } from '@angular/router'
 @Component({
   selector: 'mt-restauran-detail',
   templateUrl: './restauran-detail.component.html',
   styleUrls: ['./restauran-detail.component.css']
 })
+
 export class RestauranDetailComponent implements OnInit {
 
-  constructor() { }
+  restaurant: Restaurant;
+
+  constructor(
+    private restaurantsService: RestaurantService,
+    private route: ActivatedRoute) {
+
+  }
 
   ngOnInit() {
+    this.restaurantsService
+      .restaurantById(this.route.snapshot.params['id'])
+      .subscribe(restaurant => this.restaurant = restaurant);
   }
 
 }
