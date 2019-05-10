47610e00aba0fa3dbc12cc2fefe0fc700af4a27b
dudabrum <eduardadebrum94@gmail.com>
2019-05-10 15:03:00 -0300

    Componente radio button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,8 @@ import { MenuItemComponent } from './restaurants/restauran-detail/menu-item/menu
 import { ReviewsComponent } from './restaurants/restauran-detail/reviews/reviews.component';
 import { ShoppingCartService } from './restaurants/restauran-detail/shopping-cart/shopping-cart.service';
 import { OrderComponent } from './order/order.component';
+import { InputComponent } from './shared/input/input.component';
+import { RadioComponent } from './shared/radio/radio.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +37,9 @@ import { OrderComponent } from './order/order.component';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent
+    OrderComponent,
+    InputComponent,
+    RadioComponent
   ],
   imports: [
     BrowserModule,
